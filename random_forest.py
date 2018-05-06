087d11d465fa5b339b03efe904c6ef5670d61cfa
Adriano Benin <adriano.benin@gmail.com>
2018-05-05 23:24:33 -0300

    train tree

--- random_forest.py
+++ random_forest.py
@@ -17,26 +17,88 @@ import numpy as np
 INPUT_FILE = 'datasets/pima.tsv'
 
 class DecisionTree():
-    tree = {}
     instances = []
+    feature_names = []
 
-    def __init__(self, instances):
+    def __init__(self, instances, feature_names):
         self.instances = instances
+        self.feature_names = feature_names
 
     def build(self):
         #remove class column
-        features = self.instances[:, :-1]
+        feature_columns = self.instances[:, :-1]
         klasses = self.instances[:, -1]
+        features = []
+        for index, col in enumerate(feature_columns.T):
+            feature = Feature(self.feature_names[index], col)
+            features.append(feature)
 
+        self.build_recursive(features, klasses)
+
+
+    def get_best_feature(self, features, klasses):
         max_gain = 0
         best_feature = None
-        for feature in features.T:
-            gain = self.entropy_gain(feature, klasses)
+        for feature in features:
+            gain = self.entropy_gain(feature.values, klasses)
             if gain > max_gain:
                 max_gain = gain
                 best_feature = feature
 
-        print(max_gain)
+        return best_feature
+
+    def build_recursive(self, features, klasses):
+        node = Node()
+        if len(set(klasses)) ==  1:
+            node.isLeaf = True
+            node.feature = klasses[0]
+            return node
+        if len(features) == 0:
+            #@TODO get most common class
+            node.feature = 1
+            return node
+        else:
+            best_feature = self.get_best_feature(features, klasses)
+            node.feature = best_feature.name
+            #remove best feature from list
+            features = [feature for feature in features if feature.name != best_feature.name]
+
+            #left node
+            split_features = []
+            split_klasses = []
+            first_feature = True
+            for feature in features:
+                values = []
+                for i, value in enumerate(feature.values):
+                    if best_feature.values[i] <= np.mean(best_feature.values):
+                        values.append(value)
+                        if first_feature:
+                            split_klasses.append(klasses[i])
+
+                first_feature = False
+                split_feature = Feature(feature.name, values)
+                split_features.append(split_feature)
+            node.left = self.build_recursive(split_features, split_klasses)
+
+            #right node @TODO DRY
+            split_features2 = []
+            split_klasses2 = []
+            first_feature2 = True
+            for feature in features:
+                values = []
+                for i, value in enumerate(feature.values):
+                    if best_feature.values[i] > np.mean(best_feature.values):
+                        values.append(value)
+                        if first_feature2:
+                            split_klasses2.append(klasses[i])
+
+                first_feature2 = False
+                split_feature = Feature(feature.name, values)
+                split_features2.append(split_feature)
+
+            node.right = self.build_recursive(split_features2, split_klasses2)
+            return node
+
 
     #@TODO categorical features
     def entropy_gain(self, feature, klasses):
@@ -57,7 +119,8 @@ class DecisionTree():
             info_feature_partition = (total_partition/total)
             info_sum = 0
             for klass in klasses_set:
-                total_in_klass = sum([1 for instance in feature_with_klass if instance[1] == klass and partition_func(instance[0], mean)])
+                total_in_klass = sum([1 for instance in feature_with_klass if instance[1] == klass
+                                      and partition_func(instance[0], mean)])
                 if total_in_klass != 0 and total_partition != 0:
                     info_sum -= (total_in_klass/total_partition)*math.log(total_in_klass/total_partition, 2)
 
@@ -67,38 +130,47 @@ class DecisionTree():
         gain = info_d - info_feature
         return gain
 
+class Feature():
+    name = ""
+    values = []
+    def __init__(self, name, values):
+        self.name = name
+        self.values = values
+
+
 class Node():
+    is_leaf = False
     left = None
     right = None
     feature = ""
 
-    def __init__(self, feature, left, right):
+    def __init__(self, feature = "", left = None, right = None):
         self.feature = feature
         self.left = left
         self.right = right
 
 def main():
-    data = np.array(load_csv(INPUT_FILE))
-    tree  = DecisionTree(data)
+    feature_names, data = load_csv(INPUT_FILE, '\t')
+    data = np.array(data)
+    tree  = DecisionTree(data, feature_names)
     tree.build()
 
 
-def load_csv(filename):
+def load_csv(filename, separator= ','):
     data = []
 
     with open(filename) as file:
-        file.readline()  # skip first line
+        feature_names = file.readline().split(separator)  # skip first line
         while True:
             line = file.readline()
             if not line: break
 
-            # fields = line.split(',')
-            fields = line.split('\t')
+            fields = line.split(separator)
             # read all attributes as floats, except for the class
             instance = [float(val) for val in fields[0:-1]] + [int(fields[-1])]
             data.append(instance)
 
-    return data
+    return feature_names, data
 
 
 if __name__ == '__main__':
